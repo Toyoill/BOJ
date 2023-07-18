186deb68e9f392b4bf3e2be826d74bf0a5559ba1
Toyoill <61874428+Toyoill@users.noreply.github.com>
2023-07-18 17:58:27 +0900

    [level 3] Title: 양과 늑대, Time: 0.24 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.java
@@ -1,26 +1,21 @@
-import java.util.*;
-
 class Solution {
-    int[][][] table = new int[1 << 17][18][18];
+    int ans = 0;
 
     public int solution(int[] info, int[][] edges) {
-        return solve(1, 1, 0, info, edges);
+        solve(1, 1, 0, info, edges);
+        return ans;
     }
 
-    int solve(int state, int sheep, int wolf, int[] info, int[][] edges) {
-        if (table[state][sheep][wolf] > 0) return table[state][sheep][wolf];
-
-        table[state][sheep][wolf] = sheep;
+    void solve(int state, int sheep, int wolf, int[] info, int[][] edges) {
+        ans = Math.max(sheep, ans);
 
         for (int[] edge : edges) {
             if ((state & (1 << edge[0])) != 0 && (state & (1 << edge[1])) == 0) {
                 if (info[edge[1]] == 0)
-                    table[state][sheep][wolf] = Math.max(table[state][sheep][wolf], solve(state | (1 << edge[1]), sheep + 1, wolf, info, edges));
+                    solve(state | (1 << edge[1]), sheep + 1, wolf, info, edges);
                 else if (sheep > wolf + 1)
-                    table[state][sheep][wolf] = Math.max(table[state][sheep][wolf], solve(state | (1 << edge[1]), sheep, wolf + 1, info, edges));
+                    solve(state | (1 << edge[1]), sheep, wolf + 1, info, edges);
             }
         }
-
-        return table[state][sheep][wolf];
     }
 }
